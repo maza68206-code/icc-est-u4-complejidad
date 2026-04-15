71c227a4faf76bf3c8f7470f172994a8cc04afe5
Miguel Maza <maza68206@gmail.com>
2026-04-15 13:08:26 -0500

    Ejemplo

--- src/App.java
+++ src/App.java
@@ -2,38 +2,43 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int cantidadEstudiantes = 2000000;
+        int cantidadEstudiantes = 6000000;
         int cantidadConsultas = 40_000;
 
         System.out.println("Generando estudiantes...");
         List<Estudiante> estudiantes = Generador.generarEstudiantes(cantidadEstudiantes);
 
-        // BUSCAR SI EXISTE EL USR (búsqueda lineal)
-        String cedulaBuscar = "51_999_993";
-        for (Estudiante est : estudiantes) {
+        
+        // Es como decir est1, est2, est3
+         for (Estudiante est : estudiantes) {
+            System.out.println(est.nombre);
+         }
+
+         String cedulaBuscar= "5_999_993";
+         for (Estudiante est : estudiantes){
             if (est.cedula.equals(cedulaBuscar)) {
                 System.out.println("EXISTE");
-                break;
+                 break;
             }
-        }
+         }
 
-        // BÚSQUEDA BINARIA
-        // int inicio = 0;
-        // int fin = estudiantes.size() - 1;
+        
+        int inicio = 0;
+        int fin = estudiantes.size() - 1;
 
-        // while (inicio <= fin) {
-        //     int medio = inicio + (fin - inicio) / 2;
-        //     Estudiante estudianteMedio = estudiantes.get(medio);
-        //     int comparacion = estudianteMedio.cedula.compareTo(cedulaBuscar);
+        while (inicio <= fin) {
+            int medio =  (inicio + fin) / 2;
+            Estudiante actual = estudiantes.get(medio);
+            int comparacion = actual.cedula.compareTo(cedulaBuscar);
 
-        //     if (comparacion == 0) {
-        //         System.out.println("EXISTE (binaria)");
-        //         break;
-        //     } else if (comparacion < 0) {
-        //         inicio = medio + 1;
-        //     } else {
-        //         fin = medio - 1;
-        //     }
+            if (comparacion == 0) {
+                System.out.println("EXISTE");
+                break;
+            } else if (comparacion < 0) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
         }
     }
 }
